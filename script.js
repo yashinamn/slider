c6cb59a058f6855303ccb9139ec062229e5e6dbd
yashinamn <enigm@list.ru>
2020-04-12 00:13:11 +0300

    10 photo; slick-slider; iQwery; htlm; arrows

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:false,
+        initialSlide:0,
+        autoplay:true,
+        autoplaySpeed:500
+    });
+});
